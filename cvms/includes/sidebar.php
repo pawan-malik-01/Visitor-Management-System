6161c82f6fadcaa26e6ebdc500c2eb9fd1f0f7f1
Pawan Malik <124669097+pawan-malik-01@users.noreply.github.com>
2023-09-02 17:53:05 +0530

    Add files via upload
    committed all source code files

---
+++ cvms/includes/sidebar.php
@@ -0,0 +1,41 @@
+<aside class="menu-sidebar d-none d-lg-block" >
+            <div class="logo">
+        
+                   <h1 style="color:#fff">CVMS</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: #FFFF">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: #FFFF">
+                                <i class="fa fa-user"></i>New Visitor</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: #FFFF">
+                                <i class="fa fa-users"></i>Manage Visitors</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: #FFFF">
+                                <i class="fas fa-copy"></i>Vistors B/w Dates</a>
+                        </li>  
+
+                        <li>
+                            <a href="createid.php"  style="color: #FFFF">
+                                <i class="fas fa-id-card"></i>Create Vistors ID-card</a>
+                        </li>  
+                       
+                    </ul>
+                    <div style=" margin-top: 250px;">
+                CopyRight@2022
+            </div>
+                </nav>
+            </div>
+            
+        </aside>
